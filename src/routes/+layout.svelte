8f7d315a75652505ce6b89bb3dd8fc06eccd1825
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-09 12:37:50 +0330

    feat: build background component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Background from '$lib/components/background.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="relative isolate min-h-screen">
+	<Background />
+	{@render children()}
+</div>

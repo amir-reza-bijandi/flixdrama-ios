a54156852dad216f97956dcf0648efe6bb7057fe
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-09 13:40:54 +0330

    refactor: branding into a separate component

---
+++ src/routes/_lib/components/branding.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { ASSETS } from '$lib/assets';
+</script>
+
+<div class="relative">
+	<img src={ASSETS.FLIXDRAMA} alt="" />
+	<img class="absolute -top-6.25 -left-4.25 w-32 opacity-10" src={ASSETS.LOGO} alt="" />
+</div>
--- src/routes/_lib/components/header.svelte
+++ src/routes/_lib/components/header.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { ASSETS } from '$lib/assets';
+	import Branding from './branding.svelte';
 </script>
 
-<div class="px-7 pt-9">
-	<img class="translate-y-px" src={ASSETS.FLIXDRAMA} alt="" />
-	<img class="absolute top-3 left-3 w-32 opacity-10" src={ASSETS.LOGO} alt="" />
+<div class="relative px-7 pt-9">
+	<Branding />
 </div>

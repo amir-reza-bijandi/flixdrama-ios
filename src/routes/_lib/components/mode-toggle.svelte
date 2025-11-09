ae0474f2b377baa4a9d2fed10a8b07f1678985f5
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-09 16:14:49 +0330

    feat: use solid icons for mode toggle

--- src/routes/_lib/components/mode-toggle.svelte
+++ src/routes/_lib/components/mode-toggle.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-	import { MoonIcon, SunIcon } from '@lucide/svelte';
+	import { Moon as MoonIcon, Sun as SunIcon } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 	import { mode, toggleMode } from 'mode-watcher';
 </script>
 
 <button class="relative" onclick={toggleMode}>
-	<SunIcon
+	<Icon
+		src={SunIcon}
+		theme="solid"
 		class={[
-			'absolute top-1/2 left-1/2 -translate-1/2 text-accent-secondary-tint transition-[opacity,scale,rotate] duration-1000 ease-overshoot-light',
+			'absolute top-1/2 left-1/2 size-6 -translate-1/2 text-accent-secondary-tint transition-[opacity,scale,rotate] duration-1000 ease-overshoot-light',
 			mode.current === 'dark' ? 'scale-100 rotate-0' : 'scale-0 -rotate-360 opacity-0'
 		]}
 	/>
-	<MoonIcon
+	<Icon
+		src={MoonIcon}
+		theme="solid"
 		class={[
-			'absolute top-1/2 left-1/2 -translate-1/2 text-accent-primary-tint transition-[opacity,scale,rotate] duration-1000 ease-overshoot-light',
+			'absolute top-1/2 left-1/2 size-6 -translate-1/2 text-accent-primary-tint transition-[opacity,scale,rotate] duration-1000 ease-overshoot-light',
 			mode.current === 'light' ? 'scale-100 rotate-0' : 'scale-0 -rotate-360 opacity-0'
 		]}
 	/>

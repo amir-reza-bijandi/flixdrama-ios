92a8d9b41d3809a620a6faf0525d7422a334b668
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-11 13:37:35 +0330

    fix: prevent layout shift when branding images are getting loaded

--- src/routes/_lib/components/branding.svelte
+++ src/routes/_lib/components/branding.svelte
@@ -3,11 +3,14 @@
 </script>
 
 <div class="relative">
-	<img src={ASSETS.FLIXDRAMA} alt="" />
+	<img src={ASSETS.FLIXDRAMA} alt="" width="106" height="18" fetchpriority="high" />
 	<img
 		style="mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))"
 		class="absolute -top-5 -left-3.25 -z-10 w-32 opacity-10"
 		src={ASSETS.LOGO}
 		alt=""
+		width="106"
+		height="110"
+		fetchpriority="high"
 	/>
 </div>

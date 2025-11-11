f2b0dcf681ae829a8d6a3effeb048243947ab62b
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-11 14:42:13 +0330

    fix: content overflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
 		disableTransitions={false}
 	/>
 	<Background />
-	<div class="mx-auto max-w-lg pt-9">
+	<div class="mx-auto max-w-lg overflow-hidden pt-9">
 		<Header />
 		{@render children()}
 	</div>

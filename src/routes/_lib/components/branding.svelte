c18c81dd4e9fcc2a2daa378c35ed7f0362847495
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-11 12:02:50 +0330

    fix: prevent branding logo to be visiable on recomendations-carousel

--- src/routes/_lib/components/branding.svelte
+++ src/routes/_lib/components/branding.svelte
@@ -4,5 +4,10 @@
 
 <div class="relative">
 	<img src={ASSETS.FLIXDRAMA} alt="" />
-	<img class="absolute -top-5 -left-3.25 -z-10 w-32 opacity-10" src={ASSETS.LOGO} alt="" />
+	<img
+		style="mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))"
+		class="absolute -top-5 -left-3.25 -z-10 w-32 opacity-10"
+		src={ASSETS.LOGO}
+		alt=""
+	/>
 </div>

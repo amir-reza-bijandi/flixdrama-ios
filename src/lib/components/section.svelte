8912f8c31d679c18df4a9b769dc02d3dc19f8afe
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-11 13:27:38 +0330

    feat: build section component

---
+++ src/lib/components/section.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+	const { children }: Props = $props();
+</script>
+
+<div class="mt-6 px-5">
+	{@render children()}
+</div>

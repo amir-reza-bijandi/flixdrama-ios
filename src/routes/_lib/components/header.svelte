2a0ce0fb24b660d4d8fc6bfef4388ab3dbc37d0c
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2025-11-13 12:06:16 +0330

    feat: replace branding with back button in media page

--- src/routes/_lib/components/header.svelte
+++ src/routes/_lib/components/header.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import IconButton from '$lib/components/icon-button.svelte';
+	import { ChevronLeft } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 	import Branding from './branding.svelte';
 	import ModeToggle from './mode-toggle.svelte';
+
+	const isMediaRoute = $derived(page.url.pathname.includes(resolve('/media')));
+	const handleBack = () => window.history.back();
 </script>
 
 <div class="relative z-10 flex items-center justify-between px-7">
-	<Branding />
+	{#if !isMediaRoute}
+		<Branding />
+	{:else}
+		<IconButton onclick={handleBack}>
+			<Icon class="size-6" src={ChevronLeft} theme="mini" />
+		</IconButton>
+	{/if}
 	<ModeToggle />
 </div>

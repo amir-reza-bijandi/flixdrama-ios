2daa3d2f62447f4bcda47559423ea6b8984fa299
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2026-01-06 15:53:11 +0330

    feat: improve page icon mode switch transition

--- src/routes/_lib/components/page-icon.svelte
+++ src/routes/_lib/components/page-icon.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Image from '$lib/components/image.svelte';
 	import { TRANSITION } from '$lib/constants/transition';
+	import { toRem } from '$lib/utilities/general';
+	import { mode } from 'mode-watcher';
 	import { backOut } from 'svelte/easing';
-	import type { TransitionConfig } from 'svelte/transition';
+	import { fade, type TransitionConfig } from 'svelte/transition';
 	import { pageIconStore } from '../store/page-icon-store.svelte';
 
 	interface SpinScaleOptions {
@@ -38,16 +40,37 @@
 			}
 		};
 	}
+
+	let width = $state(0);
+	let height = $state(0);
 </script>
 
 {#key pageIconStore.current}
 	<div
-		class="fixed top-content-padding left-content-padding -z-10 mix-blend-overlay"
+		style:--width={toRem(width)}
+		style:--height={toRem(height)}
+		class="fixed top-content-padding left-content-padding -z-10 h-(--height) w-(--width) mix-blend-overlay"
 		transition:spinScale
 	>
-		<Image
-			class="size-40 mask-linear-180 mask-linear-from-foreground-primary/25 mask-linear-to-transparent"
-			src={pageIconStore.current}
-		/>
+		{#key mode.current === 'dark'}
+			<div
+				class="absolute top-0 left-0"
+				transition:fade={{ duration: TRANSITION.DURATION }}
+				bind:clientWidth={width}
+				bind:clientHeight={height}
+			>
+				{@render image(mode.current === 'dark')}
+			</div>
+		{/key}
 	</div>
 {/key}
+
+{#snippet image(isInverted: boolean)}
+	<Image
+		class={[
+			'size-40 mask-linear-180 mask-linear-from-foreground-primary/25 mask-linear-to-transparent',
+			isInverted && 'invert-100'
+		]}
+		src={pageIconStore.current}
+	/>
+{/snippet}

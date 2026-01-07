d4b698fa332e27f7c98568f9f43bef497c3d2a9f
Amir Reza Bijandi <amir.reza.bijandi@outlook.com>
2026-01-07 09:43:11 +0330

    feat: add touch feedback for playlists

--- src/routes/fa/_lib/components/playlists.svelte
+++ src/routes/fa/_lib/components/playlists.svelte
@@ -11,6 +11,7 @@
 <script lang="ts">
 	import { Box } from '$lib/components/box';
 	import Image from '$lib/components/image.svelte';
+	import { Pressable } from '$lib/components/pressable';
 	import { cn, type ClassValue } from 'tailwind-variants';
 
 	type Props = {
@@ -22,23 +23,25 @@
 
 <div class={cn('flex flex-col gap-2', extraClass)}>
 	{#each data as { title, subtitle, href, images }}
-		<a class="block" {href}>
-			<Box.Root class="flex items-center justify-between p-4">
-				<Box.Visuals class="rounded-2xl bg-background-tertiary" />
-				<div class="flex flex-col gap-2">
-					<div class={['text-sm leading-none', subtitle && 'font-bold']}>{title}</div>
-					{#if subtitle}
-						<div class="text-xs leading-none text-foreground-secondary">{subtitle}</div>
-					{/if}
-				</div>
-				<div
-					class="flex w-44.5 gap-1.5 mask-linear-90 mask-linear-from-black mask-linear-to-transparent"
-				>
-					{#each images as image}
-						<Image class="aspect-2/3 w-10 rounded-lg" src={image} hasLoadingGlow hasOutline />
-					{/each}
-				</div>
-			</Box.Root>
-		</a>
+		<Pressable.Root as="a" class="block" {href}>
+			<Pressable.Content>
+				<Box.Root class="flex items-center justify-between p-4">
+					<Box.Visuals class="rounded-2xl bg-background-tertiary" />
+					<div class="flex flex-col gap-2">
+						<div class={['text-sm leading-none', subtitle && 'font-bold']}>{title}</div>
+						{#if subtitle}
+							<div class="text-xs leading-none text-foreground-secondary">{subtitle}</div>
+						{/if}
+					</div>
+					<div
+						class="flex w-44.5 gap-1.5 mask-linear-90 mask-linear-from-black mask-linear-to-transparent"
+					>
+						{#each images as image}
+							<Image class="aspect-2/3 w-10 rounded-lg" src={image} hasLoadingGlow hasOutline />
+						{/each}
+					</div>
+				</Box.Root>
+			</Pressable.Content>
+		</Pressable.Root>
 	{/each}
 </div>
